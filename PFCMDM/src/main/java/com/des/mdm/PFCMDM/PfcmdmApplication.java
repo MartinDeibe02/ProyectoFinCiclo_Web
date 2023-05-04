e4781e4cc02ae7c053de9da4e6c9a6913d9ac62f
Martin <martindeibe@gmail.com>
2023-05-04 10:49:01 +0200

    Creando proyecto

---
+++ PFCMDM/src/main/java/com/des/mdm/PFCMDM/PfcmdmApplication.java
@@ -0,0 +1,13 @@
+package com.des.mdm.PFCMDM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PfcmdmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PfcmdmApplication.class, args);
+	}
+
+}
---
+++ PFCMDM/src/test/java/com/des/mdm/PFCMDM/PfcmdmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.des.mdm.PFCMDM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PfcmdmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
