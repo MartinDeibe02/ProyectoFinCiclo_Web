011ef894bf07ac2a630b4612ecac4045cf52a5e6
Martin <martindeibe@gmail.com>
2023-05-11 13:42:59 +0200

    Cambios en admin

---
+++ PFCMDM/src/main/resources/static/js/sidebar.js
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+         window.onload = function () {
+            const sidebar = document.querySelector(".sidebar");
+            const closeBtn = document.querySelector("#btn");
+
+            closeBtn.addEventListener("click", function () {
+                sidebar.classList.toggle("open")
+                menuBtnChange()
+            })
+
+        }
