81b02c0ed8e5b66295c8c485bc6a38f4a934f351
Martin <martindeibe@gmail.com>
2023-05-23 11:14:11 +0200

    Control errores register

--- PFCMDM/src/main/java/com/des/mdm/PFCMDM/Controller/UsersController.java
+++ PFCMDM/src/main/java/com/des/mdm/PFCMDM/Controller/UsersController.java
@@ -3,10 +3,13 @@ package com.des.mdm.PFCMDM.Controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.des.mdm.PFCMDM.Service.UserService;
 import com.des.mdm.PFCMDM.model.Permisos;
@@ -33,7 +36,13 @@ public class UsersController {
 	}
 	
 	@PostMapping("/saveUser")
-	public String saveUsers(User user) {
+	public String saveUsers(User user, BindingResult result, RedirectAttributes redirectAttributes) {
+		if(result.hasErrors()) {
+			for(ObjectError error : result.getAllErrors()) {
+				System.out.println("Error: " + error.getDefaultMessage());
+			}
+			return "redirect:/register";
+		}
 		user.setContrasena(passwordEncoder.encode(user.getContrasena()));
 		user.setEstatus(1);
 		
@@ -41,7 +50,8 @@ public class UsersController {
 		permiso.setId(2);
 		user.agregarPermiso(permiso);
 		userService.saveUser(user);
-		
+        redirectAttributes.addFlashAttribute("suc", "¡Registro completado!");
+
 		return  "redirect:/login";
 	}
 	
